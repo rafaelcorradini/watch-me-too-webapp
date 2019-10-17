63345d9c4e5646a162c68554a17c88170824eb37
Rafael Corradini da Cunha <corradini@angularlabs.com.br>
2019-10-17 13:48:32 -0300

    icon and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import style from './App.scss';
+import './App.scss';
 import Room from './components/Room/Room';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <Router>
-      <div className={style.app}>
+      <div className="app">
         <Route path="/" exact component={Home} />
         <Route path="/room/:roomId" exact component={Room} />
       </div>
